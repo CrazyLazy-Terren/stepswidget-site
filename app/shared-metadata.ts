bb3369f516fbb36d1b8cc7aed9bacd71d79c80c2
CrazyLazy <crazylazy007@gmail.com>
2026-07-03 17:19:31 +0800

    Update og

---
+++ app/shared-metadata.ts
@@ -0,0 +1,11 @@
+export const siteName = 'Steps Widget'
+export const siteUrl = 'https://steps.crazylazy.xyz'
+
+export const defaultOgImages = [
+  {
+    url: 'https://static.crazylazy.xyz/pic/f01.png',
+    width: 1600,
+    height: 1080,
+    alt: 'Steps Widget preview across iPhone and Apple Watch',
+  },
+]
