1fb27bc3d6523e1312ec551521b16e488fb5f28e
CrazyLazy <crazylazy007@gmail.com>
2026-07-03 14:07:31 +0800

    fix sitemap

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/blog/posts/', '/blog/posts/*'],
+      disallow: ['/terms', '/privacy'],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   }
